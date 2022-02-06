2025553746a71bac09f05a14ed2ec1ff77534eac
Wunax <wunax@protonmail.com>
2022-02-05 21:53:43 -0500

    fix: error if route has no meta

--- client/middleware/auth.js
+++ client/middleware/auth.js
@@ -1,6 +1,6 @@
 export default function ({ app, route, redirect }) {
   const loggedIn = app.$cookies.get('loggedIn') === 'true';
-  if (route.meta[0].notLoggedIn && loggedIn) {
+  if (route?.meta[0]?.notLoggedIn && loggedIn) {
     return redirect('/');
   }
   if (!loggedIn && route.path !== '/login' && route.path !== '/register') {
